c8eaca808ed2ef7be63dda9074f298ef11880e6b
simku22 <116035879+simku22@users.noreply.github.com>
2023-04-22 20:31:08 -0700

    v1.0 - Added First Draft
    Added current HTML/CSS/JS files to start development

---
+++ js/JavaScript.js
@@ -0,0 +1,14 @@
+﻿<!-- Swiper JS -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js">
+    </script>
+
+<!-- Initialize Swiper -->
+<script>
+    var swiper = new Swiper(".mySwiper", {
+        direction: "vertical",
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+</script>
